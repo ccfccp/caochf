e91fa7938ea9270f331321ca2114ae6caf6cdb12
caochf <93766164@qq.com>
2020-04-06 22:53:54 +0800

    zzz

---
+++ c_01_meta/src/main/java/com/ccf/c_01_meta/C01MetaApplication.java
@@ -0,0 +1,13 @@
+package com.ccf.c_01_meta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C01MetaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(C01MetaApplication.class, args);
+    }
+
+}
---
+++ c_01_meta/src/test/java/com/ccf/c_01_meta/C01MetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccf.c_01_meta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C01MetaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
