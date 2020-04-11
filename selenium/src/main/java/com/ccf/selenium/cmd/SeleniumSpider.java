5a74b6ad170e2ca75a45dab3d6e2012c6c310222
曹成峰 <caochf@inspur.com>
2020-04-11 09:08:33 +0800

    zzz

--- selenium/src/main/java/com/ccf/selenium/cmd/SeleniumSpider.java
+++ selenium/src/main/java/com/ccf/selenium/cmd/SeleniumSpider.java
@@ -3,6 +3,8 @@ package com.ccf.selenium.cmd;
 public class SeleniumSpider {
     private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(SeleniumSpider.class);
     public static void main(String[] args){
+        log.debug("start!");
 
+        log.debug("end!");
     }
 }
